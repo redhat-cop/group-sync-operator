8bb07ef9b771b6af7d0095ceab5f8aba2fc74c24
Sebastian Widmer <sebastian.widmer@vshn.net>
2021-10-29 12:34:13 +0200

    Add tests for different mappings

--- pkg/syncer/keycloak.go
+++ pkg/syncer/keycloak.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
-	"strings"
 
 	"crypto/x509"
 
@@ -16,10 +15,8 @@ import (
 	"github.com/redhat-cop/group-sync-operator/pkg/constants"
 	"github.com/redhat-cop/operator-utils/pkg/util"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
-	"k8s.io/apimachinery/pkg/util/validation"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
@@ -63,6 +60,11 @@ func (k *KeycloakSyncer) Init() bool {
 
 	if k.Provider.Scope == "" {
 		k.Provider.Scope = redhatcopv1alpha1.SubSyncScope
+		changed = true
+	}
+
+	if k.Provider.SubGroupProcessing == "" {
+		k.Provider.SubGroupProcessing = redhatcopv1alpha1.FlatSubGroupProcessing
 		changed = true
 	}
 
@@ -174,169 +176,30 @@ func (k *KeycloakSyncer) Sync() ([]userv1.Group, error) {
 		return nil, err
 	}
 
-	for _, group := range groups {
+	mapper := KeycloakGroupMapper{
+		GetGroupMembers: k.getGroupMembers,
 
-		if _, groupFound := k.CachedGroups[*group.ID]; !groupFound {
-			k.processGroupsAndMembers(group, nil, k.Provider.Scope, k.Provider.SubGroupProcessing, k.Provider.SubJoinSeparator)
-		}
+		AllowedGroups:      k.Provider.Groups,
+		Scope:              k.Provider.Scope,
+		SubGroupProcessing: k.Provider.SubGroupProcessing,
+		SubJoinSeparator:   k.Provider.SubJoinSeparator,
 	}
 
-	ocpGroups := []userv1.Group{}
+	ocpGroups, err := mapper.Map(groups)
+	if err != nil {
+		return nil, fmt.Errorf("error mapping keycloak groups: %w", err)
+	}
 
-	for _, cachedGroup := range k.CachedGroups {
+	url, err := url.Parse(k.Provider.URL)
+	if err != nil {
+		return nil, err
+	}
 
-		groupAttributes := map[string]string{}
-
-		for key, value := range cachedGroup.Attributes {
-			// we add the annotation that qualify for OCP annotations and log for the ones that don't
-			if errs := validation.IsQualifiedName(key); len(errs) == 0 {
-				groupAttributes[key] = strings.Join(value, "'")
-			} else {
-				keycloakLogger.Info("unable to add annotation to", "group", cachedGroup.Name, "key", key, "value", value)
-			}
-		}
-
-		ocpGroup := userv1.Group{
-			TypeMeta: v1.TypeMeta{
-				Kind:       "Group",
-				APIVersion: userv1.GroupVersion.String(),
-			},
-			ObjectMeta: v1.ObjectMeta{
-				Name:        *cachedGroup.Name,
-				Annotations: groupAttributes,
-				Labels:      map[string]string{},
-			},
-			Users: []string{},
-		}
-
-		url, err := url.Parse(k.Provider.URL)
-
-		if err != nil {
-			return nil, err
-		}
-
-		childrenGroups := []string{}
-
-		for _, subgroup := range cachedGroup.SubGroups {
-			childrenGroups = append(childrenGroups, *subgroup.Name)
-		}
-
-		parentGroups := []string{}
-
-		for _, group := range k.CachedGroups {
-			for _, subgroup := range group.SubGroups {
-				if *subgroup.Name == *cachedGroup.Name {
-					parentGroups = append(parentGroups, *group.Name)
-				}
-			}
-		}
-
-		// Set Host Specific Details
-		ocpGroup.GetAnnotations()[constants.SyncSourceHost] = url.Host
-		ocpGroup.GetAnnotations()[constants.SyncSourceUID] = *cachedGroup.ID
-		if len(childrenGroups) > 0 {
-			ocpGroup.GetAnnotations()[constants.HierarchyChildren] = strings.Join(childrenGroups, ",")
-		}
-		if len(parentGroups) == 1 {
-			ocpGroup.GetAnnotations()[constants.HierarchyParent] = parentGroups[0]
-		}
-		if len(parentGroups) > 1 {
-			ocpGroup.GetAnnotations()[constants.HierarchyParents] = strings.Join(parentGroups, ",")
-		}
-
-		if redhatcopv1alpha1.JoinSubGroupProcessing == k.Provider.SubGroupProcessing {
-			candidates := make([]*gocloak.Group, 0, len(k.CachedGroups))
-			for _, group := range k.CachedGroups {
-				candidates = append(candidates, group)
-			}
-			parents := findAllParentGroups(cachedGroup, candidates)
-
-			path := make([]string, 0, len(parents)+1)
-			for _, parent := range parents {
-				path = append(path, *parent.Name)
-			}
-			path = append(path, *cachedGroup.Name)
-
-			ocpGroup.Name = strings.Join(path, k.Provider.SubJoinSeparator)
-		}
-
-		for _, user := range k.CachedGroupMembers[*cachedGroup.ID] {
-			ocpGroup.Users = append(ocpGroup.Users, *user.Username)
-		}
-
-		ocpGroups = append(ocpGroups, ocpGroup)
-
+	for _, g := range ocpGroups {
+		g.GetAnnotations()[constants.SyncSourceHost] = url.Host
 	}
 
 	return ocpGroups, nil
-}
-
-func (k *KeycloakSyncer) processGroupsAndMembers(group, parentGroup *gocloak.Group, scope redhatcopv1alpha1.SyncScope, subGroupProcessing redhatcopv1alpha1.SubGroupProcessing, subJoinSeparator string) error {
-
-	if parentGroup == nil && !isGroupAllowed(*group.Name, k.Provider.Groups) {
-		return nil
-	}
-
-	if redhatcopv1alpha1.JoinSubGroupProcessing == subGroupProcessing &&
-		subJoinSeparator != "" &&
-		strings.Contains(*group.Name, subJoinSeparator) {
-		keycloakLogger.Error(
-			errGroupNameContainsSeparator,
-			"error processing group",
-			"group", *group.Name,
-			"separator", subJoinSeparator,
-		)
-		return errGroupNameContainsSeparator
-	}
-
-	k.CachedGroups[*group.ID] = group
-
-	groupMembers, err := k.getGroupMembers(*group.ID)
-
-	if err != nil {
-		return err
-	}
-
-	k.CachedGroupMembers[*group.ID] = groupMembers
-
-	// Add Group Members to Primary Group
-	if parentGroup != nil && redhatcopv1alpha1.JoinSubGroupProcessing != subGroupProcessing {
-		usersToAdd, _ := k.diff(groupMembers, k.CachedGroupMembers[*parentGroup.ID])
-		k.CachedGroupMembers[*parentGroup.ID] = append(k.CachedGroupMembers[*parentGroup.ID], usersToAdd...)
-	}
-
-	// Process Subgroups
-	if redhatcopv1alpha1.SubSyncScope == scope {
-		for _, subGroup := range group.SubGroups {
-			if _, subGroupFound := k.CachedGroups[*subGroup.ID]; !subGroupFound {
-				k.processGroupsAndMembers(subGroup, group, scope, subGroupProcessing, subJoinSeparator)
-			}
-		}
-	}
-
-	return nil
-}
-
-func (k *KeycloakSyncer) diff(lhsSlice, rhsSlice []*gocloak.User) (lhsOnly []*gocloak.User, rhsOnly []*gocloak.User) {
-	return k.singleDiff(lhsSlice, rhsSlice), k.singleDiff(rhsSlice, lhsSlice)
-}
-
-func (k *KeycloakSyncer) singleDiff(lhsSlice, rhsSlice []*gocloak.User) (lhsOnly []*gocloak.User) {
-	for _, lhs := range lhsSlice {
-		found := false
-		for _, rhs := range rhsSlice {
-			if *lhs.ID == *rhs.ID {
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			lhsOnly = append(lhsOnly, lhs)
-		}
-	}
-
-	return lhsOnly
 }
 
 func (k *KeycloakSyncer) getGroups() ([]*gocloak.Group, error) {
@@ -395,36 +258,4 @@ func (k *KeycloakSyncer) getGroupMembers(groupId string) ([]*gocloak.User, error
 
 func (k *KeycloakSyncer) GetProviderName() string {
 	return k.Name
-}
-
-func findParentGroup(group *gocloak.Group, candidates []*gocloak.Group) *gocloak.Group {
-	for _, candidate := range candidates {
-		for _, subgroup := range candidate.SubGroups {
-			if *subgroup.ID == *group.ID {
-				return candidate
-			}
-		}
-	}
-
-	return nil
-}
-
-func findAllParentGroups(group *gocloak.Group, candidates []*gocloak.Group) []*gocloak.Group {
-	parents := []*gocloak.Group{}
-
-	for {
-		parent := findParentGroup(group, candidates)
-		if parent == nil {
-			break
-		}
-
-		parents = append(parents, parent)
-		group = parent
-	}
-
-	for l, r := 0, len(parents)-1; l < r; l, r = l+1, r-1 {
-		parents[l], parents[r] = parents[r], parents[l]
-	}
-
-	return parents
 }
